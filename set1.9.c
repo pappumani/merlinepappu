e51f9e2265bfac126db7feb8a84a24651958d4fc
pappumani <30828059+pappumani@users.noreply.github.com>
2018-01-10 11:07:18 +0000

    Create set1.9.c

---
+++ set1.9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int avl;
+int bvl;
+int i;
+printf("enter the value for a,b\n");
+scanf("%d%d",&avl,&bvl);
+for(i=0;i<=aval;i++)
+{
+printf("%d",&avl);
+}
+if(bvl<0)
+{
+bvl=i+bvl;
+printf("%d",bvl);
+}
+else
+{
+printf("enter the b value");
+}
+return 0;
+}
