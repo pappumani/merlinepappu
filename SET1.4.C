cf87c0d80ec4e1eca484b55158b6f1eba1ec121e
pappumani <30828059+pappumani@users.noreply.github.com>
2018-01-10 10:45:08 +0530

    Create SET1.4.C

---
+++ SET1.4.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+char a;
+printf("Enter character: ");
+scanf("%c",&a);
+if( (a>='a' && a<='z') || (a>='A' && a<='Z'))
+{
+printf("%c is an alphabet.",a);
+}
+else
+{
+printf("%c is not an alphabet.",a);
+}
+}
