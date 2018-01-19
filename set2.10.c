f212c2dcbb69b5e1063398920c3b1acf4a87abf0
pappumani <30828059+pappumani@users.noreply.github.com>
2018-01-19 11:02:26 +0000

    Create set2.10.c

---
+++ set2.10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int a,n,temp;
+	printf("enter the limit\n");
+	scanf("%d",&a);
+	for(n=1;n<=a;n++)
+    if(a!=0)
+	{
+   temp=5*n;
+   printf("%d\t",temp);
+	}
+	}
