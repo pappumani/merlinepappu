546b25158af7bab45bb2c692d01d01cc9876d206
pappumani <30828059+pappumani@users.noreply.github.com>
2018-01-21 16:40:21 +0530

    Create set4.3.c

---
+++ set4.3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+char a[20];
+int b,spc=0;
+printf("enter the string");
+scanf("%s",&a[i]);
+while(a[b]!='\0')
+{
+if(!(a[b]>='a')&&(a[b]<='z')))
+{
+spc=spc+1;
+}
+b++;
+}
+printf("%d",spc);
+}
