b7c60fa0851393a46abc8ab9a524616c681b8c91
pappumani <30828059+pappumani@users.noreply.github.com>
2018-01-21 16:36:38 +0530

    Create set4.5.c

---
+++ set4.5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+char m[30];
+int a=0,
+printf("Enter any string\n");
+scanf("%s",&m);
+if(m==1||m==9)
+{
+a=a++;
+printf("the no of charcter in string is %d\n",a)
+}
