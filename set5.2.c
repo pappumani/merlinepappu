fe2003f7bdb2bf43a5383b137a81e9cb48ec7c29
pappumani <30828059+pappumani@users.noreply.github.com>
2018-01-24 19:08:33 +0530

    Create set5.2.c

---
+++ set5.2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main()
+{
+char a[20],b[20];
+int i,j,count1,count2;
+for(i=0;i<n;i++)
+{
+scanf("%s",&a[i]);
+count1+=1;
+}
+for(j=0;j<n;j++)
+{
+scanf("%s",&b[j]);
+count2+=1;
+}
+if(count1==count2)
+printf("%s",a[i]);
+else
+if(count1<count2)
+{
+printf("%s",b[i]);
+}
+else
+{
+printf("%s",a[i]);
+}
+}
