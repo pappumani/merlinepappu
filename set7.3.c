8bf9869679b44e97b070bd35157743db83b4eb3b
pappumani <30828059+pappumani@users.noreply.github.com>
2018-02-05 16:35:06 +0530

    Create set7.3.c

---
+++ set7.3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a[20];
+int i,b=1;
+printf("enter the string\n");
+scanf("%s",&a[i]);
+for(i=0;a[i]!='\0';i++)
+{
+    if(a[i]==' ')
+    {
+   ++b;
+    }
+}
+printf("%d",++b);
+}
