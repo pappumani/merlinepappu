ba1cae51b27f149b03a3aff3b4597522687dea49
pappumani <30828059+pappumani@users.noreply.github.com>
2018-02-12 16:01:49 +0530

    Create set8.3.c

---
+++ set8.3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(b>a&&c>b)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
