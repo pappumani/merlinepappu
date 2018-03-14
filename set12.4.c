c71dd3a7173218cbe579f5a9a4ed895b27932bac
pappumani <30828059+pappumani@users.noreply.github.com>
2018-03-14 16:30:13 +0530

    Create set12.4.c

---
+++ set12.4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int n,k,r[10],i,t=0;
+	scanf("%d",&n);
+	scanf("%d\n",&n);
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&r[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+            if(r[i]%2!=k)
+            {
+            t+=1;
+            }
+	}
+	       if(t==1);
+	{
+	    printf("%d",r[i]);
+	   
+	}
+            
+}
