a9b85503d26f66fcbf7b49c45996636a98879cd1
pappumani <30828059+pappumani@users.noreply.github.com>
2018-03-19 10:52:30 +0000

    Create pset1.6.c

---
+++ pset1.6.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int main()
+{
+    char a[6];
+    scanf("%s",a);
+}
+switch(a)
+{
+    case 'I':pritf("1");
+    break;
+    case 'I''I':pritf("2");
+    break;
+    case 'I''I''I':pritf("3");
+    break;
+    case 'I''V':pritf("4");
+    break;
+    case 'V':pritf("5");
+    break;
+    case 'V''I':pritf("6");
+    break;
+    case 'V''I''I':pritf("7");
+    break;
+    case 'V''I''I''I':pritf("8");
+    break;
+    case 'I''X':pritf("9");
+    break;
+    case 'X':pritf("10");
+    break;
+    case 'X''I':pritf("11");
+    break;
+    case 'X''I''I':pritf("12");
+    break;
+    case 'X''I''I''I':pritf("13");
+    break;
+    case 'X''I''V':pritf("14");
+    break;
+    case 'X''V':pritf("15");
+    break;
+    case 'X''V''I':pritf("16");
+    break;
+    case 'X''V''I''I':pritf("17");
+    break;
+    case 'X''V''I''I''I':pritf("18");
+    break;
+    case 'X''X''I':pritf("19");
+    break;
+    case 'X''X''X':pritf("20");
+    break;
+    default:
+    printf("try again");
+}
